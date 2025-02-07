4c0d308e5006c6bc415dcfef7271bcb69d3c9d17
IppSec <IppSec@users.noreply.github.com>
2025-02-07 11:42:27 -0500

    Make Burpsuite Compatible with Parrot6

--- roles/customize-browser/files/getburpcert.sh
+++ roles/customize-browser/files/getburpcert.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-/bin/bash -c "timeout 45 /usr/lib/jvm/java-17-openjdk-amd64/bin/java -Djava.awt.headless=true -jar /usr/share/burpsuite/burpsuite.jar < <(echo y) &" 
+burp=$(find / -name burp*.jar 2>/dev/null | tail -1)
+/bin/bash -c "timeout 45 /usr/share/burpsuite/jre/bin/java -Djava.awt.headless=true -jar $burp < <(echo y) &" 
 sleep 30
 curl http://localhost:8080/cert -o /tmp/cacert.der
 exit
+
